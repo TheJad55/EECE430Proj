faf6ec42ba9d68a7fe6063c028bdde0013367220
TheJad55 <jjs19@mail.aub.edu>
2023-04-13 23:59:04 +0300

    Revert "Addition of registration page and login page."
    This reverts commit ecd8488bfe2a9c88eb0b00a973b7ea8abb83844f.

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Navbar2 from '../navbar/Navbar2'
-import LoginForm from '../usableComponents/LoginForm';
 
 const Login = () => {
   return (
     <div>
       <Navbar2 />
       <h1>Login Page</h1>
-      <LoginForm />
-      
     </div>
-  );    
+  );
 }
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "slick-carousel/slick/slick.css";
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
