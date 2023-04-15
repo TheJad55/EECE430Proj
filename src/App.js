93477d40b2fdefa505c37e1dd83cd200b559fa55
TheJad55 <jjs19@mail.aub.edu>
2023-04-15 15:16:27 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Footer from "./components/footer/Footer";
 import FooterBottom from "./components/footer/FooterBottom";
-import Navbar from "./components/navbar/Navbar";
 import Homep from "./components/home/Homep";
 import Login from "./components/login/Login";
 import Register from "./components/register/Registration";
