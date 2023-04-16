121a5c3ae8e8d61ce1cc52d7d4a9f11ab62d243e
TheJad55 <jjs19@mail.aub.edu>
2023-04-16 19:21:06 +0300

    fix

--- src/components/homesignedin/homesignedin.js
+++ src/components/homesignedin/homesignedin.js
@@ -9,20 +9,18 @@ const Homesignedin = () => {
     <div>
       <Navbar3 />
       <div className="max-w-screen-xl mx-auto">
-        <div className="w-full py-20 border-b-[1px] border-b-black">
+
           <PlayerProfile />
-        </div>
         <h1 className="text-6xl font-bold text-white mt-8">
           Your <span className="text-designColor capitalize">Stats</span>
         </h1>
-        <div className="w-full py-20 border-b-[1px] border-b-black">
+
           <div className="m-8">
             <PlayerStatsTable />
             <div className="m-8">
               <Calendar3 />
             </div>
           </div>
-        </div>
       </div>
     </div>
   );
