c948c34047036814ec271c7e68bab50e0d14c02f
TheJad55 <jjs19@mail.aub.edu>
2023-04-18 11:04:05 +0300

    coach components

---
+++ src/components/contact/ContactCoach.js
@@ -0,0 +1,185 @@
+import React,{useState} from 'react'
+import Title from '../layouts/Title';
+import ContactCoachLeft from './ContactCoachLeft';
+
+const ContactCoach = () => {
+  const [username, setUsername] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
+
+  // ========== Email Validation start here ==============
+  const emailValidation = () => {
+    return String(email)
+      .toLocaleLowerCase()
+      .match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/);
+  };
+  // ========== Email Validation end here ================
+  const isPositiveInteger = (value) => /^\d+$/.test(value);
+
+  const handleSend = (e) => {
+    e.preventDefault();
+    if (username === "") {
+      setErrMsg("Player name is required!");
+    } else if (username.trim().length < 2) {
+      setErrMsg("Player name must be at least 2 characters long!");
+    } else if (phoneNumber === "" || !isPositiveInteger(phoneNumber)) {
+      setErrMsg("Valid game number is required!");
+    } else if (email === "" || !isPositiveInteger(email)) {
+      setErrMsg("Valid points are required!");
+    } else if (subject === "" || !isPositiveInteger(subject)) {
+      setErrMsg("Valid rebounds are required!");
+    } else if (message === "" || !isPositiveInteger(message)) {
+      setErrMsg("Valid assists are required!");
+    } else {
+      setSuccessMsg(
+        `Thank you dear ${username}, Your Messages has been sent Successfully!`
+      );
+      setErrMsg("");
+      setUsername("");
+      setPhoneNumber("");
+      setEmail("");
+      setSubject("");
+      setMessage("");
+    }
+  };
+  
+  return (
+    <section
+      id="contact"
+      className="w-full py-20 border-b-[1px] border-b-black"
+    >
+      <div className="flex justify-center items-center text-center">
+        <Title title="CONTACT" des="Contact Our Team" />
+      </div>
+      <div className="w-full">
+        <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
+          <ContactCoachLeft />
+          <div className="w-full lgl:w-[60%] h-full py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] flex flex-col gap-8 p-4 lgl:p-8 rounded-lg shadow-shadowOne">
+            <form className="w-full flex flex-col gap-4 lgl:gap-6 py-2 lgl:py-5">
+              {errMsg && (
+                <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce">
+                  {errMsg}
+                </p>
+              )}
+              {successMsg && (
+                <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-green-500 text-base tracking-wide animate-bounce">
+                  {successMsg}
+                </p>
+              )}
+              <div className="w-full flex flex-col lgl:flex-row gap-10">
+                <div className="w-full lgl:w-1/2 flex flex-col gap-4">
+                  <p className="text-sm text-gray-400 uppercase tracking-wide">
+                    Player Name
+                  </p>
+                  <input
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                    className={`${
+                      errMsg === "Username is required!" &&
+                      "outline-designColor"
+                    } contactInput`}
+                    type="text"
+                  />
+                </div>
+                <div className="w-full lgl:w-1/2 flex flex-col gap-4">
+                  <p className="text-sm text-gray-400 uppercase tracking-wide">
+                    Game Number
+                  </p>
+                  <input
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    value={phoneNumber}
+                    className={`${
+                      errMsg === "Phone number is required!" &&
+                      "outline-designColor"
+                    } contactInput`}
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-4">
+                <p className="text-sm text-gray-400 uppercase tracking-wide">
+                  Points
+                </p>
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  className={`${
+                    errMsg === "Please give your Email!" &&
+                    "outline-designColor"
+                  } contactInput`}
+                  type="email"
+                />
+              </div>
+              <div className="flex flex-col gap-4">
+                <p className="text-sm text-gray-400 uppercase tracking-wide">
+                  Rebounds
+                </p>
+                <input
+                  onChange={(e) => setSubject(e.target.value)}
+                  value={subject}
+                  className={`${
+                    errMsg === "Plese give your Subject!" &&
+                    "outline-designColor"
+                  } contactInput`}
+                  type="text"
+                />
+              </div>
+              <div className="flex flex-col gap-4">
+                <p className="text-sm text-gray-400 uppercase tracking-wide">
+                  Assists
+                </p>
+                <input
+                  onChange={(e) => setSubject(e.target.value)}
+                  value={subject}
+                  className={`${
+                    errMsg === "Plese give your Subject!" &&
+                    "outline-designColor"
+                  } contactInput`}
+                  type="text"
+                />
+              </div>
+              <div className="flex flex-col gap-4">
+                <p className="text-sm text-gray-400 uppercase tracking-wide">
+                  Steals
+                </p>
+                <input
+                  onChange={(e) => setSubject(e.target.value)}
+                  value={subject}
+                  className={`${
+                    errMsg === "Plese give your Subject!" &&
+                    "outline-designColor"
+                  } contactInput`}
+                  type="text"
+                />
+              </div>
+              <div className="w-full">
+                <button
+                  onClick={handleSend}
+                  className="w-full h-12 bg-[#141518] rounded-lg text-base text-gray-400 tracking-wider uppercase hover:text-white duration-300 hover:border-[1px] hover:border-designColor border-transparent"
+                >
+                  Blocks
+                </button>
+              </div>
+              {errMsg && (
+                <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce">
+                  {errMsg}
+                </p>
+              )}
+              {successMsg && (
+                <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-green-500 text-base tracking-wide animate-bounce">
+                  {successMsg}
+                </p>
+              )}
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ContactCoach;
