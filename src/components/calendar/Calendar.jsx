04b67da32c64b68d897ad37564a398ee45f04546
TheJad55 <jjs19@mail.aub.edu>
2023-05-04 01:49:10 +0300

    calendar fun

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
+import { startOfDay, endOfDay } from 'date-fns';
+import React, { useState, useEffect } from "react";
 import {
   Box,
   List,
@@ -14,114 +15,240 @@ import {
 } from "@mui/material";
 import Header from "./Header";
 import { tokens } from "./theme";
+import { color } from "framer-motion";
 
 const Calendar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const [currentEvents, setCurrentEvents] = useState([]);
 
-  const handleDateClick = (selected) => {
+  // Fetch events from the backend
+  const fetchEvents = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/event", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + sessionStorage.getItem("access_token"),
+        },
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+  
+        // Set a minimum duration of 30 minutes for events with the same start and end times
+        const updatedData = data.map((event) => {
+          if (event.EventStart === event.EventEnd) {
+            const startDate = new Date(event.EventStart);
+            const endDate = new Date(startDate);
+            endDate.setMinutes(startDate.getMinutes() + 30);
+            event.EventEnd = endDate.toISOString();
+          }
+          return event;
+        });
+  
+        setCurrentEvents(updatedData);
+      } else {
+        console.error("Failed to fetch events");
+      }
+    } catch (error) {
+      console.error("Error fetching events:", error);
+    }
+  };
+  
+  
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  const handleDateClick = async (selected) => {
     const title = prompt("Please enter a new title for your event");
     const calendarApi = selected.view.calendar;
     calendarApi.unselect();
-
+  
     if (title) {
-      calendarApi.addEvent({
-        id: `${selected.dateStr}-${title}`,
-        title,
-        start: selected.startStr,
-        end: selected.endStr,
-        allDay: selected.allDay,
-      });
+      const startDate = new Date(selected.startStr);
+      const endDate = new Date(startDate);
+      const isAllDay = selected.allDay;
+  
+      if (isAllDay) {
+        endDate.setDate(startDate.getDate() + 1);
+        startDate.setHours(0, 0, 0, 0);
+        endDate.setHours(0, 0, 0, 0);
+      } else {
+        endDate.setMinutes(startDate.getMinutes() + 60);
+      }
+  
+      const endStr = endDate.toISOString();
+  
+      const newEvent = {
+        EventName: title,
+        EventType: "Custom",
+        Team: "Lebron",
+        EventStart: new Date(startDate).toISOString(),
+        EventEnd: new Date(endStr).toISOString(),
+        EventParticipants: "AllTeam",
+        EventOrganizer: "Botros",
+        allDay: isAllDay,
+      };
+  
+      try {
+        const response = await fetch("http://127.0.0.1:8000/event", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + sessionStorage.getItem("access_token"),
+          },
+          body: JSON.stringify(newEvent),
+        });
+  
+        if (response.ok) {
+          fetchEvents();
+        } else {
+          console.error("Failed to post new event");
+        }
+      } catch (error) {
+        console.error("Error posting new event:", error);
+      }
     }
   };
+  
+  
 
-  const handleEventClick = (selected) => {
+
+  const handleEventClick = async (info) => {
     if (
       window.confirm(
-        `Are you sure you want to delete the event '${selected.event.title}'`
+        `Are you sure you want to delete the event '${info.event.title}'`
       )
     ) {
-      selected.event.remove();
+      info.event.remove(); // Remove the event from the calendar interface immediately
+
+      try {
+        const eventId = info.event.id;
+        const response = await fetch(
+          `http://127.0.0.1:8000/delete/event/${eventId}`,
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + sessionStorage.getItem("access_token"),
+            },
+          }
+        );
+        fetchEvents();
+
+        if (!response.ok) {
+          console.error("Failed to delete the event");
+          // Handle the error case, such as re-adding the event to the calendar or showing an error message
+        }
+      } catch (error) {
+        console.error("Error deleting the event:", error);
+        // Handle the error case, such as re-adding the event to the calendar or showing an error message
+      }
     }
   };
-
+  
+  
+  
   return (
-    <section
-    id="calendar"
-    className="w-full py-20 border-b-[1px] border-b-black"
-  >
-    <Box m="20px">
-      <Header title="Calendar" subtitle="Full Calendar Interactive Page" />
-
-      <Box display="flex" justifyContent="space-between">
-        {/* CALENDAR SIDEBAR */}
-        <Box
-          flex="1 1 20%"
-          backgroundColor={colors.primary[400]}
-          p="15px"
-          borderRadius="4px"
-        >
-          <Typography variant="h5">Events</Typography>
-          <List>
-            {currentEvents.map((event) => (
-              <ListItem
-                key={event.id}
-                sx={{
-                  backgroundColor: colors.greenAccent[500],
-                  margin: "10px 0",
-                  borderRadius: "2px",
-                }}
-              >
-                <ListItemText
-                  primary={event.title}
-                  secondary={
-                    <Typography>
-                      {new Date(event.start).toLocaleString("default", {
-                        year: "numeric",
-                        month: "short",
-                        day: "numeric",
-                      })}
-                    </Typography>
-                  }
-                />
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-
-        {/* CALENDAR */}
-        <Box flex="1 1 100%" ml="15px">
+    <section id="calendar" className="w-full py-20 border-b-[1px] border-b-black">
+      <Box m="20px">
+        <Header title="Calendar" subtitle="Full Calendar Interactive Page" />
+  
+        <Box display="flex" justifyContent="space-between">
+          {/* CALENDAR SIDEBAR */}
+          <Box
+            flex="1 1 20%"
+            backgroundColor={colors.primary[400]}
+            p="15px"
+            borderRadius="4px"
+          >
+            <Typography variant="h5">Events</Typography>
+            <List>
+              {currentEvents.map((event) => (
+                <ListItem
+                  key={event.id}
+                  sx={{
+                    backgroundColor: " #2d3748",
+                    Color:" #2d3748",
+                    textColor:"white",
+                    margin: "10px 0",
+                    borderRadius: "2px",
+                  }}
+                >
+                  <ListItemText
+                    primary={event.title}
+                    secondary={
+                      <Typography>
+                        {new Date(event.start).toLocaleString("default", {
+                          year: "numeric",
+                          month: "short",
+                          day: "numeric",
+                        })}
+                      </Typography>
+                    }
+                  />
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+  
+          {/* CALENDAR */}
+          <Box flex="1 1 100%" ml="15px">
           <FullCalendar
-            height="75vh"
-            plugins={[
-              dayGridPlugin,
-              timeGridPlugin,
-              interactionPlugin,
-              listPlugin,
-            ]}
-            headerToolbar={{
-              left: "prev,next today",
-              center: "title",
-              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
-            }}
-            initialView="dayGridMonth"
-            editable={true}
-            selectable={true}
-            selectMirror={true}
-            dayMaxEvents={true}
-            select={handleDateClick}
-            eventClick={handleEventClick}
-            eventsSet={(events) => setCurrentEvents(events)}
-            initialEvents={[
+          height="75vh"
+          plugins={[
+            dayGridPlugin,
+            timeGridPlugin,
+            interactionPlugin,
+            listPlugin,
+          ]}
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+          }}
+          initialView="dayGridMonth"
+          editable={false}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          select={(selected) => {
+            const isAllDay = selected.allDay;
+            handleDateClick(selected, isAllDay);
+          }}
+          eventClick={handleEventClick}
+          eventsSet={(events) => {
+            if (events.length !== currentEvents.length) {
+              setCurrentEvents(events);
+            }
+          }}
+          events={currentEvents.map((event) => ({
+            id: event.id,
+            title: event.EventName,
+            start: event.EventStart,
+            end: event.EventEnd,
+            allDay: event.allDay || false,
+            backgroundColor:"#2d3748",
+            color:" #2d3748"
+          }))}
+          
+          selectConstraint={{
+            minutes: 60,
+          }}
+          eventResizableFromStart={false} 
+          eventResizableFromEnd={false} 
+          eventDraggable={false} 
+        />
 
-            ]}
-          />
+          </Box>
         </Box>
       </Box>
-    </Box>
     </section>
   );
-};
+ };
 
 export default Calendar;
+          
--- src/components/calendar/Calendar3.jsx
+++ src/components/calendar/Calendar3.jsx
@@ -1,17 +1,9 @@
-import { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
-import {
-  Box,
-  List,
-  ListItem,
-  ListItemText,
-  Typography,
-  useTheme,
-} from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Box, Typography, useTheme } from "@mui/material";
 import Header from "./Header";
 import { tokens } from "./theme";
 
@@ -20,100 +12,85 @@ const Calendar = () => {
   const colors = tokens(theme.palette.mode);
   const [currentEvents, setCurrentEvents] = useState([]);
 
-  const handleDateClick = (selected) => {
-    const title = prompt("Please enter a new title for your event");
-    const calendarApi = selected.view.calendar;
-    calendarApi.unselect();
-
-    if (title) {
-      calendarApi.addEvent({
-        id: `${selected.dateStr}-${title}`,
-        title,
-        start: selected.startStr,
-        end: selected.endStr,
-        allDay: selected.allDay,
+  // Fetch events from the backend
+  const fetchEvents = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/event", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + sessionStorage.getItem("access_token"),
+        },
       });
+  
+      if (response.ok) {
+        const data = await response.json();
+  
+        // Set a minimum duration of 30 minutes for events with the same start and end times
+        const updatedData = data.map((event) => {
+          if (event.EventStart === event.EventEnd) {
+            const startDate = new Date(event.EventStart);
+            const endDate = new Date(startDate);
+            endDate.setMinutes(startDate.getMinutes() + 30);
+            event.EventEnd = endDate.toISOString();
+          }
+          return event;
+        });
+  
+        setCurrentEvents(updatedData);
+      } else {
+        console.error("Failed to fetch events");
+      }
+    } catch (error) {
+      console.error("Error fetching events:", error);
     }
   };
+  
+  
 
-  const handleEventClick = (selected) => {
-    if (
-      window.confirm(
-        `Are you sure you want to delete the event '${selected.event.title}'`
-      )
-    ) {
-      selected.event.remove();
-    }
-  };
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+  useEffect(() => {
+    fetchEvents();
+  }, []);
 
   return (
-    <section
-    id="calendar"
-    className="w-full py-20 border-b-[1px] border-b-black"
-  >
-    <Box m="20px">
-      <Header title="Calendar" subtitle="Full Calendar Interactive Page" />
+    <section id="calendar" className="w-full py-20 border-b-[1px] border-b-black">
+      <Box m="20px">
+        <Header title="Calendar" subtitle="Full Calendar Interactive Page" />
 
-      <Box display="flex" justifyContent="space-between">
-        {/* CALENDAR SIDEBAR */}
-        <Box
-          flex="1 1 20%"
-          backgroundColor={colors.primary[400]}
-          p="15px"
-          borderRadius="4px"
-        >
-          <Typography variant="h5">Events</Typography>
-          <List>
-            {currentEvents.map((event) => (
-              <ListItem
-                key={event.id}
-                sx={{
-                  backgroundColor: colors.greenAccent[500],
-                  margin: "10px 0",
-                  borderRadius: "2px",
-                }}
-              >
-                <ListItemText
-                  primary={event.title}
-                  secondary={
-                    <Typography>
-                      {new Date(event.start).toLocaleString("default", {
-                        year: "numeric",
-                        month: "short",
-                        day: "numeric",
-                      })}
-                    </Typography>
-                  }
-                />
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-
-        {/* CALENDAR */}
-        <Box flex="1 1 100%" ml="15px">
-          <FullCalendar
-            height="75vh"
-            plugins={[
-              dayGridPlugin,
-              timeGridPlugin,
-              interactionPlugin,
-              listPlugin,
-            ]}
-            headerToolbar={{
-              left: "prev,next today",
-              center: "title",
-              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
-            }}
-            initialView="dayGridMonth"
-            dayMaxEvents={true}
-            eventsSet={(events) => setCurrentEvents(events)}
-            initialEvents={[
-            ]}
-          />
+        <Box display="flex" justifyContent="space-between">
+          {/* CALENDAR */}
+          <Box flex="1 1 100%" ml="15px">
+            <FullCalendar
+              height="75vh"
+              plugins={[
+                dayGridPlugin,
+                timeGridPlugin,
+                listPlugin,
+              ]}
+              headerToolbar={{
+                left: "prev,next today",
+                center: "title",
+                right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+              }}
+              initialView="dayGridMonth"
+              editable={false}
+              events={currentEvents.map((event) => ({
+                id: event.id,
+                title: event.EventName,
+                start: new Date(event.EventStart).toISOString(),
+                end: new Date(event.EventEnd).toISOString(),
+                allDay: event.allDay || false,
+                backgroundColor: "#2d3748",
+                borderColor: "#2d3748",
+                textColor: "white",
+              }))}
+            />
+          </Box>
         </Box>
       </Box>
-    </Box>
     </section>
   );
 };
