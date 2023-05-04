dd7bf57b0b809fa9b3fc6a87d01141abc8465b9d
CarlAziz <cca07@mail.aub.edu>
2023-05-04 11:47:33 +0300

    Fixed Navbars

--- src/components/navbar/Navbar3.js
+++ src/components/navbar/Navbar3.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-scroll";
 import { FiMenu } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
 import { logo } from "../../assets/index";
+import { navLinksdata } from "../../constants";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
